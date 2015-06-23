a4e83e4430412bd0b489dd024b1e3629f3554525
SteveDoyle2 <mesheb82@gmail.com>
2015-06-22 20:33:06 -0700

    updating make adding some files

--- MYSTRAN-FEA-Program/build_make.py
+++ MYSTRAN-FEA-Program/build_make.py
@@ -1,6 +1,6 @@
 import os
 
-def get_files_of_type(dirname, extension='.txt', maxSize=100., recursive=False):
+def get_files_of_type(dirname, extensions='.txt', maxSize=100., recursive=False):
     """
     Gets the list of all the files with a given extension in the specified directory
 
@@ -9,30 +9,87 @@ def get_files_of_type(dirname, extension='.txt', maxSize=100., recursive=False):
     :param maxSize:   size in MB for max file size
     :returns: list of all the files with a given extension in the specified directory
     """
+    if isinstance(extensions, str):
+        extensions = [extensions]
     filenames = []
     if recursive:
         for f in os.listdir(dirname):
             dirname_file = os.path.join(dirname, f)
+            if f.startswith('_'):
+                print('ignoring %s' % dirname_file)
+                continue
             if os.path.isdir(dirname_file):
-                filenames += get_files_of_type(dirname_file, extension, recursive=recursive)
+                filenames += get_files_of_type(dirname_file, extensions, recursive=recursive)
             else:
-                if os.path.splitext(f)[1].endswith(extension):
-                    filenames.append(os.path.join(dirname, f))
+                for extension in extensions:
+                    if extension in ['mod']:
+                        print(f)
+                    if os.path.splitext(f)[1].endswith(extension):
+                        filenames.append(os.path.join(dirname, f))
+                        break
         return filenames
     else:
         if not os.path.exists(dirname):
             return []
         return [os.path.join(dirname, f) for f in os.listdir(dirname)
-                if os.path.splitext(f)[1].endswith(extension)
+                if os.path.splitext(f)[1].endswith(extensions[0])
                  and os.path.getsize(os.path.join(dirname, f)) / (1048576.) <= maxSize]
 
-src_dir = os.path.join('1-SRC', 'a-Windows-version')
-f90_files = get_files_of_type(src_dir, 'f90', recursive=True)
+def main():
+    src_dir_base = os.path.join('1-SRC', 'a-Windows-version')
 
-f90_files2 = [fname.replace('\\', '\\\\') for fname in f90_files]
-build_msg = 'gfortran -o mystran ' + ' '.join(f90_files2)
-#for fname in f90_files:
-#    print(fname)
+    #f90_files = get_files_of_type(src_dir, ['f90', 'f'], recursive=True)
 
-with open('build.sh', 'w') as f:
-    f.write(build_msg)
+    dirs = [
+        'BANDIT', 'EMG',
+        'LK1', 'LK2', 'LK3', 'LK4', 'LK5', 'LK6', 'LK9',
+        'MAIN', 'UTIL',
+    ]
+    build_msg = ''
+    build_msg_mv = ''
+    all_f90_files = []
+    if not os.path.exists('mod'):
+        os.mkdir('mod')
+
+    src_dir = os.path.join(src_dir_base, 'Modules')
+    f90_files = get_files_of_type(src_dir, ['f90', 'f'], recursive=True)
+    f90_files2 = [fname.replace('\\', '/') for fname in f90_files]
+    all_f90_files += f90_files
+    for fname in f90_files2:
+        basename = os.path.basename(fname)
+        base, ext = os.path.splitext(basename)
+        #build_msg_mv += 'mv %s.mod mod/%s.mod\n' % (base, base)
+        build_msg += 'gfortran -c %s\n' % fname
+    build_msg_mv += 'mv *.mod mod/\n'
+    build_msg += build_msg_mv
+
+    for dirname in dirs:
+        src_dir = os.path.join(src_dir_base, dirname)
+        print(src_dir)
+        f90_files = get_files_of_type(src_dir, ['f90', 'f'], recursive=True)
+        f90_files2 = [fname.replace('\\', '/') for fname in f90_files]
+        for fname in f90_files2:
+            build_msg += 'gfortran -c %s -I/mod\n' % fname
+
+
+    #f90_files = get_files_of_type(src_dir, ['f90', 'f'], recursive=True)
+    #mod_files = get_files_of_type('.', extensions='.mod', maxSize=100., recursive=False)
+
+    #for f in f90_files:
+    #    print(f)
+    #for f in mod_files:
+    #    print(f)
+    #f90_files += mod_files
+
+    all_f90_obj_files = [fname.replace('\\', '/') for fname in all_f90_files]
+
+    #f90_files2 = [fname.replace('\\', '\/') for fname in f90_files]
+    build_msg += 'gfortran -o mystran ' + ' '.join(all_f90_obj_files)
+    #for fname in f90_files:
+    #    print(fname)
+
+    with open('build.sh', 'w') as f:
+        f.write(build_msg)
+
+if __name__ == '__main__':
+    main()
